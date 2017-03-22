c023715ebb131758ed5130ebc8267afaa876fdfa
kr-zephyr <scin21c@gmail.com>
2017-03-22 13:25:24 +0900

    일일 top 10 포스트 반환값에 사이트 유형 추가

--- logger/src/main/java/com/sz21c/statistics/model/TopTenHitPostDto.java
+++ logger/src/main/java/com/sz21c/statistics/model/TopTenHitPostDto.java
@@ -6,4 +6,14 @@ import lombok.Data;
 public class TopTenHitPostDto {
     private String urlTitle;
     private int count;
+    private String siteType;
+
+    public TopTenHitPostModel getTopTenHitPostModel() {
+        TopTenHitPostModel topTenHitPostModel = new TopTenHitPostModel();
+        topTenHitPostModel.setBlogType(this.siteType);
+        topTenHitPostModel.setTitle(this.urlTitle);
+        topTenHitPostModel.setViewCount(this.count);
+
+        return topTenHitPostModel;
+    }
 }
