8060022b9eb8f6c7248c9e179be3b5588784ba8b
kr-zephyr <scin21c@gmail.com>
2017-03-29 13:24:38 +0900

    Hibernate 설정 준비

---
+++ logger/src/main/java/com/sz21c/config/PersistenceHibernateConfig.java
@@ -0,0 +1,9 @@
+package com.sz21c.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceHibernateConfig {
+}
