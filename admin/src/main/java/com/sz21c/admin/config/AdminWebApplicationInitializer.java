279810e61c67764b2c0aebd1c95f2fb57b19db80
이승철 <scin21c@gmail.com>
2018-11-30 15:35:17 +0900

    Admin 툴 준비

---
+++ admin/src/main/java/com/sz21c/admin/config/AdminWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.sz21c.admin.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class AdminWebApplicationInitializer extends WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+
+    }
+}
